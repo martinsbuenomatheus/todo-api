b487c0037b1a11abcd1b7e0d4ab7ad465570d3e5
Matheus Martins Bueno <128252714+martinsbuenomatheus@users.noreply.github.com>
2024-11-14 00:24:58 -0300

    feat: readd appsettings.Development

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesApi", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
